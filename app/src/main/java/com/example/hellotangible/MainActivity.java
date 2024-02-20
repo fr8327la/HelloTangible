a9e1bd984a108f0ebb75c267385b44898832fc41
Ahmad Ghaemi <ahmad.ghaemi@hotmail.com>
2024-02-20 15:07:22 +0100

    added body of connect, publish, subscribe

--- app/src/main/java/com/example/hellotangible/MainActivity.java
+++ app/src/main/java/com/example/hellotangible/MainActivity.java
@@ -7,11 +7,16 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.*;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -150,16 +155,98 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
+
+    String inTopic = "mamn01/HelloTangibleGang/arduinoSensor";
+    String outTopic = "mamn01/HelloTangibleGang/androidSensor";
+
     public void connect() {
+        String clientId = MqttClient.generateClientId();
+        final MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+                        clientId);
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+        options.setCleanSession(false);
+        options.setUserName("VR-Lab");
+        options.setPassword("wifivrlab1".toCharArray());
+        try {
+            IMqttToken token = client.connect(options);
+            //IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d("file", "onSuccess");
+                    //publish(client,"ourMessageToArduino");
+                    subscribe(client,inTopic);
+                    client.setCallback(new MqttCallback() {
+
+                        @Override
+                        public void connectionLost(Throwable cause) {
+
+                        }
+                        @Override
+                        public void messageArrived(String topic, MqttMessage message) throws Exception {
+                            Log.d("file", message.toString());
+                            if (message.toString().equals("start")) {
+                                showColorFieldsWithDelay();
+                            }
+
+                        }
+                        @Override
+                        public void deliveryComplete(IMqttDeliveryToken token) {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d("file", "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void publish(MqttAndroidClient client, String topic, String payload) {
-
+    public void publish(MqttAndroidClient client, String payload) {
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+            client.publish(outTopic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     public void subscribe(MqttAndroidClient client, String topic) {
-        
+        int qos = 1;
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
 }
